7acdb0313d0ea4adb96f2ae0b17d35b7f7c2c2e0
mbenic <mbenic@hotmail.com>
2024-11-07 09:35:09 +0800

    carousel image size

--- app/components/CarouselImage.js
+++ app/components/CarouselImage.js
@@ -6,7 +6,7 @@ function CarouselImage({ text }) {
   // the encoding returns https://via.placeholder.com/800x400.png?text=First%20slide
   return (
     <Image
-      src={`https://via.placeholder.com/1250x500.png?text=${encodeURIComponent(
+      src={`https://via.placeholder.com/1920x768.png?text=${encodeURIComponent(
         text
       )}`}
       alt={text}
